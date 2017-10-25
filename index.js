e2e2580dd685a183c0f12040e0b6870f5e777973
soroz30 <soroz30@yahoo.pl>
2017-10-25 22:49:17 +0200

    Zadanie 13.5

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var os = require('os');
+var EventEmitter = require("events").EventEmitter;
 var OSinfo = require('./modules/OSInfo');
+
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function(instruction) {
+    console.log('You wrote: ' + instruction + ', trying to run command');
+});
+emitter.on("afterCommand", function() {
+    console.log('Finished command');
+})
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.trim();
+        emitter.emit('beforeCommand', instruction);
         switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -20,5 +29,6 @@ process.stdin.on('readable', function() {
             default:
                 process.stderr.write('Wrong instruction!\n');
         }
+        emitter.emit('afterCommand');
     }
 });
